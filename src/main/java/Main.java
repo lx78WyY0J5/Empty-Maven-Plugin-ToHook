8290ed643ea9046d693a07c5c5fc8eb419e6ca9d
lx78WyY0J5 <9j2k37st@gmail.com>
2021-09-14 15:32:48 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import org.bukkit.Bukkit;
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class Main extends JavaPlugin
+{
+    private static Logger logger = Bukkit.getLogger();
+
+    @Override
+    public void onLoad(){
+        logger.info("Plugin Loaded");
+    }
+
+    @Override
+    public void onEnable(){
+        logger.info("Plugin Enabled");
+    }
+
+    @Override
+    public void onDisable(){
+        logger.info("Plugin Disabled");
+    }
+}
